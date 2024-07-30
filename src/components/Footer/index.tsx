b9126ae2c739445c6d09d4c5e7abdd40a4d2f3d5
hanshan <hanshan@qq.com>
2024-07-30 19:58:47 +0800

    添加公安备案号

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,7 @@
 import { GithubOutlined } from '@ant-design/icons';
 import { DefaultFooter } from '@ant-design/pro-components';
 import React from 'react';
+import {Image} from "antd";
 
 const Footer: React.FC = () => {
   const defaultMessage = '寒山出品';
@@ -19,9 +20,16 @@ const Footer: React.FC = () => {
           blankTarget: true,
         },
         {
-          key: 'hanshan',
+          key: 'ICP备案',
           title: '苏ICP备2024116155号',
           href: 'https://beian.miit.gov.cn',
+          blankTarget: true,
+        },
+        {
+          key: '公安备案',
+          title: <><Image alt={"备案图标"} src={"/备案图标.png"} width={16} preview={false}/> 贵公网安备52022202000280 </>,
+          href: 'https://beian.mps.gov.cn/#/query/webSearch?code=52022202000280',
+          rel: 'noreferrer',
           blankTarget: true,
         }
       ]}
